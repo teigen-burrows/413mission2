0b7797340e3d5a94f1a5cfaa2f0615dfdc377769
teigen-burrows <131505643+teigen-burrows@users.noreply.github.com>
2024-01-18 18:35:53 -0700

    Add files via upload

---
+++ Dice.cs
@@ -0,0 +1,12 @@
+﻿public class Dice
+{
+    public int Roll()
+    {
+        // generate random rolls and store total of rolls
+        Random random = new Random();
+        int rollOne = random.Next(1, 7);
+        int rollTwo = random.Next(1, 7);
+        int rollTotal = rollOne + rollTwo;
+        return rollTotal;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.InteropServices;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // create variables
+        string input = "";
+        int number = 0;
+
+        // print string and read user input
+        Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate? ");
+        input = Console.ReadLine();
+
+        // convert user input string to an integer
+        number = Convert.ToInt32(input);
+
+        // print string based on user input
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS\nEach '*' represents 1% of the total number of rolls.\nTotal number of rolls = " + number + ".\n");
+
+        // create an array to store roll totals (my friend Burke wanted to be honored here)
+        int[] Burke = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        // for loop to load array using the Dice class/method
+        for (int i = 0; i < number; i++)
+        {
+            Dice dice = new Dice();
+            int roll = dice.Roll();
+            Burke[roll - 2]++;
+        }
+
+        // nested for loop to determine percents and print asterisks
+        for (int i = 0; i < 11; i++)
+        {
+            double count = (double)Burke[i] / number * 100;
+            int roundedCount = (int)Math.Round(count);
+            string Asterisk = "";
+            for (int j = 0; j < roundedCount; j++)
+            {
+                Asterisk += "*";
+            }
+            Console.WriteLine((i+2) + ": " + Asterisk);
+        }
+
+        // print final line
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
