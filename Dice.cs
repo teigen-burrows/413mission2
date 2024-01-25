30abaa2563ca20ac985f0a3ff4cfdbd4c99d18d1
teigen-burrows <131505643+teigen-burrows@users.noreply.github.com>
2024-01-25 13:03:10 -0700

    Update Dice.cs

--- Dice.cs
+++ Dice.cs
@@ -1,12 +1,23 @@
-﻿public class Dice
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Dice
 {
-    public int Roll()
+    public int[] Roll(int number)
     {
         // generate random rolls and store total of rolls
         Random random = new Random();
-        int rollOne = random.Next(1, 7);
-        int rollTwo = random.Next(1, 7);
-        int rollTotal = rollOne + rollTwo;
-        return rollTotal;
+
+        // create an array to store roll totals (my friend Burke wanted to be honored here)
+        int[] Burke = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        // for loop to load array using the Dice class/method
+        for (int i = 0; i < number; i++)
+        {
+            int rollOne = random.Next(1, 7);
+            int rollTwo = random.Next(1, 7);
+            int rollTotal = rollOne + rollTwo;
+            Burke[rollTotal - 2]++;
+        }
+        return Burke;
     }
 }
