f96c1645decea6074d145e795d0dce6f1205ed99
teigen-burrows <131505643+teigen-burrows@users.noreply.github.com>
2024-01-25 13:04:04 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 internal class Program
 {
     private static void Main(string[] args)
     {
+        Dice d = new Dice();
+
         // create variables
         string input = "";
         int number = 0;
+
+        int[] Teigen = new int[11];
 
         // print string and read user input
         Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate? ");
@@ -17,21 +21,12 @@ internal class Program
         // print string based on user input
         Console.WriteLine("DICE ROLLING SIMULATION RESULTS\nEach '*' represents 1% of the total number of rolls.\nTotal number of rolls = " + number + ".\n");
 
-        // create an array to store roll totals (my friend Burke wanted to be honored here)
-        int[] Burke = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-        // for loop to load array using the Dice class/method
-        for (int i = 0; i < number; i++)
-        {
-            Dice dice = new Dice();
-            int roll = dice.Roll();
-            Burke[roll - 2]++;
-        }
+        Teigen = d.Roll(number);
 
         // nested for loop to determine percents and print asterisks
         for (int i = 0; i < 11; i++)
         {
-            double count = (double)Burke[i] / number * 100;
+            double count = (double)Teigen[i] / number * 100;
             int roundedCount = (int)Math.Round(count);
             string Asterisk = "";
             for (int j = 0; j < roundedCount; j++)
